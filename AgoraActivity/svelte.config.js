b14fb6a3365ac08e2dbd853345c9bbaa789f9054
michaelbedard <courriermichaelbedard@gmail.com>
2026-01-19 11:15:28 -0500

    network 4

--- AgoraActivity/svelte.config.js
+++ AgoraActivity/svelte.config.js
@@ -11,7 +11,17 @@ const config = {
 			fallback: 'index.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ["'self'"],
+				'script-src': ["'self'", "'unsafe-inline'"], // Unity often needs unsafe-inline
+				'connect-src': ["'self'", "https://api.agoraboardgames.com", "https://*.discordsays.com"],
+				'img-src': ["'self'", "data:", "blob:"],
+				'style-src': ["'self'", "'unsafe-inline'"]
+			}
+		}
 	}
 };
 
